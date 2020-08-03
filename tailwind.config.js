5a1300468c0e1afe9fe368f4d0ef3681694bcb4f
Josh Preuss <josh.preuss@datarobot.com>
2020-08-03 08:50:54 -0400

    frontend work

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+    colors: {
+      pink: '#CC9B8B',
+      blue: '#758ECD',
+      lightblue: '#C3CEEA',
+      mediumblue:'#A5B4DF', 
+      white: '#eeeeff'
+    }},
+  },
+  variants: {},
+  plugins: [],
+}
